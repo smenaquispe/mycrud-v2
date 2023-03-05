868937b3ba6df76f5ac591a125e084105f99e5b5
smenaq <sergiosantosmenaq@gmail.com>
2023-03-05 16:28:00 -0500

    adding cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,14 @@ import morgan from 'morgan'
 import { config } from 'dotenv';
 import mongoose from 'mongoose'
 import router from './routes/tasks.routes.js'
+import cors from 'cors'
 
 config();
 
 const app = express();
 const port = process.env.PORT || 9000;
 
+app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
