7e253f8f2f5bfabb8610e8b90d0a85362946bf1e
smenaq <sergiosantosmenaq@gmail.com>
2023-03-16 12:08:23 -0500

    delete task is done

--- app/src/components/TaskTable.jsx
+++ app/src/components/TaskTable.jsx
@@ -1,17 +1,27 @@
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { doAction } from '../utils/apiFunctions.js'
+import { useNavigate } from 'react-router-dom'
 
 function TaskTable(){
 
     const [tasks, setTasks] = useState(null)
-    
+    const navigate = useNavigate()
     
     // obtener la lista de tasks
     useEffect(() => {
         doAction()
         .then(setTasks)
     }, [])
+
+
+    // eliminar un task
+    const deleteTask = id => {
+        doAction('delete', JSON.stringify({id}))
+        .then(console.log)
+        .then(navigate(0))
+    }
+
 
     if(!tasks)
     return <h1>Loading...</h1>
@@ -39,7 +49,7 @@ function TaskTable(){
                         <td>{task.date}</td>
                         <td>
                         <Link to={`/edit/${task._id}`} ><button>Edit</button></Link> 
-                        <button>Delete</button>
+                        <button onClick={e => deleteTask(task._id)}>Delete</button>
                         <button>Done</button>
                         </td>
                     </tr>
