7c462ee4455ad3e82a13ae355b180f1ee3cec2e1
smenaq <sergiosantosmenaq@gmail.com>
2023-03-20 20:34:09 -0500

    just detail with done

--- app/src/components/Task.jsx
+++ app/src/components/Task.jsx
@@ -3,7 +3,7 @@ import DoneCheckBox from './DoneCheckBox'
 
 function Task({task}){
     return (
-        <tr key={task._id} id={task._id}>
+        <tr key={task._id} id={task._id} className={task.done && 'line'}>
             <td>{task.name}</td>
             <td>{task.description}</td>
             <td>{task.date}</td>
